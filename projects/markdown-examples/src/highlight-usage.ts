8e3cfaff8d54be4b0aab9573502b5b87aa216d81
KostyaTretyak <ktretiak.in.ua@gmail.com>
2023-08-06 21:02:05 +0300

    Upgrade deps

--- projects/markdown-examples/src/highlight-usage.ts
+++ projects/markdown-examples/src/highlight-usage.ts
@@ -1,6 +1,6 @@
-import { highlight } from 'highlight.js';
+import hljs from 'highlight.js';
 import { Marked } from '@ts-stack/markdown';
 
-Marked.setOptions({ highlight: (code, lang) => highlight(lang, code).value });
+Marked.setOptions({ highlight: (code, lang) => hljs.highlight(lang, code).value });
 
 console.log(Marked.parse('```js\n console.log("hello"); \n```'));
